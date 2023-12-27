f60521256a0a66de548fd522cde4c710bae87f18
Fabian Juren <fjuren@gmail.com>
2023-12-26 17:32:58 -0800

    Add updated API endpoint for development and production env and updated post.service

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -1,17 +1,18 @@
 import axios from 'axios';
 import { authHeader } from './auth-header';
+import { API_URL } from '../utils/api';
 
-const API_URL = 'http://localhost:3000/api/posts';
+console.log(API_URL);
 
 export const getTimelinePosts = (page: number) => {
-  return axios.get(API_URL + `/timeline?page=${page}`, {
+  return axios.get(API_URL + `/posts/timeline?page=${page}`, {
     headers: authHeader(),
   });
 };
 
 export const timelinePost = (postData: FormData) => {
   return axios.post(
-    API_URL + '/create_post',
+    API_URL + '/posts/create_post',
     // { content, image }, // keeping this syntax as reminder of curly braces
     postData, // contains content (as string) and file
     { headers: authHeader() }
@@ -19,12 +20,14 @@ export const timelinePost = (postData: FormData) => {
 };
 
 export const getUserProfilePosts = (userID: string) => {
-  return axios.get(API_URL + `/profile/${userID}`, { headers: authHeader() });
+  return axios.get(API_URL + `/posts/profile/${userID}`, {
+    headers: authHeader(),
+  });
 };
 
 export const likePost = (postID: string) => {
   return axios.post(
-    API_URL + '/like_post',
+    API_URL + '/posts/like_post',
     { postID: postID },
     { headers: authHeader() }
   );

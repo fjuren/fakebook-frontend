2efff35e09b55660418cc7d735dc9f43c804180d
Fabian Juren <fjuren@gmail.com>
2023-12-29 12:33:25 -0800

    Add note

--- src/services/updateProfilePic.ts
+++ src/services/updateProfilePic.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { authHeader } from './auth-header';
-import { API_URL } from './user.service';
+// import { API_URL } from './user.service';
+import { API_URL } from '../utils/api'; //TODO this needs fixing
 
 // update profile pic on profile page
 
